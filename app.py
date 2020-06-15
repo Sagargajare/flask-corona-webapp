cc0d085bab8d7f767c687ba2cd1e199805789321
Sagargajare <56780589+Sagargajare@users.noreply.github.com>
2020-06-15 14:14:06 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+
+extract_contents = lambda row: [x.text.replace('\n', '') for x in row]
+URL = 'https://www.mohfw.gov.in/'
+
+
+
+response = requests.get(URL).content
+soup = BeautifulSoup(response, 'html.parser')
+header = extract_contents(soup.tr.find_all('th'))
+
+
+
+all_rows = soup.find_all('tr')
+def func():
+    stats = []
+    for row in all_rows:
+        stat = extract_contents(row.find_all('td'))
+        # print(stat)
+
+        if stat:
+            if len(stat) == 6:
+                # last row
+                stat = ['', *stat]
+                stats.append(stat)
+            elif len(stat) == 5:
+                stats.append(stat)
+    return stats
+
+
+
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    stats1 = func()
+
+    return render_template('index.html',a = stats1)
+
+
+app.run()
